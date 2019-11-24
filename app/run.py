e6e9cc3ab4ade30d8cf03f13a8179590f2cfa1e7
pierelucas <pierelucas@keemail.me>
2019-11-24 19:49:03 +0100

    run

--- app/run.py
+++ app/run.py
@@ -2,10 +2,34 @@
 # MIT LICENSE
 
 
+import time
 from app import *
 
 
 class RunClass():
+
+    def __init__(self):
+        
+        self.lt = time.localtime()
+        
+        self.author = "Julian Huch"
+        self.license = "MIT LICENSE"
+        self.version = "1.0"
+        
+        banner_txt = """
+        ---------------------
+        PyPassword - Manager
+        Coding by %s
+            Version: %s
+                %s
+        ---------------------
+        """ % (self.author, self.version, self.license)
+
+        menu_txt = """
+        [1] Write entry to DB
+        [2] Read entry's
+        [3] Delete entry's
+        """
 
     def __repr__(self):
         return "%s" % self.__class__.__name__
@@ -17,7 +41,7 @@ class RunClass():
         pass
 
     def run(self):
-        pass
+        
 
 
 
